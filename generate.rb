64dd01275cd2018ab3d011bf8f27a0446032c80d
Mark Berger <maberger6@gmail.com>
2016-03-08 22:18:05 -0500

    Added Sass processing for html stylesheet.

--- generate.rb
+++ generate.rb
@@ -2,6 +2,7 @@
 
 require 'json'
 require 'liquid'
+require 'sass'
 
 require_relative 'tags/fixed_width'
 require_relative 'tags/justify_date'
@@ -15,20 +16,43 @@ def load_json(filename)
   end
 end
 
-def render_template(ext)
-  tmp_file = File.read("templates/#{ext}/resume.#{ext}.liquid")
+def render_txt
+  tmp_file = File.read('templates/txt/resume.txt.liquid')
   template = Liquid::Template.parse(tmp_file)
-  template.render('resume' => @json_resume)
-end
 
-load_json('resume.json')
-extensions = %w(html tex txt)
-
-Dir.mkdir('out') unless File.exist?('out') && File.directory?('out')
-
-extensions.each do |e|
-  temp = render_template(e)
-  File.open('out/' + "resume.#{e}", 'w') do |file|
-    file.write temp
+  File.open('out/resume.txt', 'w') do |file|
+    file.write template.render('resume' => @json_resume)
   end
 end
+
+def render_html
+  tmp_file = File.read('templates/html/resume.html.liquid')
+  template = Liquid::Template.parse(tmp_file)
+
+  scss = File.read('templates/html/style.scss')
+  css = Sass::Engine.new(scss, syntax: :scss).render
+
+  File.open('out/resume.html', 'w') do |file|
+    file.write template.render('resume' => @json_resume, 'style' => css)
+  end
+end
+
+def render_tex
+  tmp_file = File.read('templates/tex/resume.tex.liquid')
+  template = Liquid::Template.parse(tmp_file)
+
+  File.open('out/resume.tex', 'w') do |file|
+    file.write template.render('resume' => @json_resume)
+  end
+end
+
+def generate
+  Dir.mkdir('out') unless File.exist?('out') && File.directory?('out')
+
+  load_json('resume.json')
+  render_txt
+  render_html
+  render_tex
+end
+
+generate
